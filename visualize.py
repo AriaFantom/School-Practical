4bdf4e28c5cc6cb5b70a78b419680ac5926144c5
AriaFantom <priyanshudebnath348@gmail.com>
2022-03-09 18:52:39 +0530

    Added Search with ID

--- visualize.py
+++ visualize.py
@@ -16,6 +16,13 @@ if dbname not in [x[0] for x in db.fetchall()]:
     exit()
 else:
     choice = input("Enter your choice: ")
+    print("""
+    1. Show the Salary as Department wise Graph
+    2. Show the percentage of teachers as Department wise Pie Chart
+    3. Show the slary of teacher
+    4. Show all the Data from a Dataframe
+    5. Search for a teacher with ID
+    """)
     if choice == "1":
         db.execute("USE " + dbname)
         db.execute("SELECT department, salary FROM " + dbname)
@@ -37,5 +44,18 @@ else:
         df = pd.read_sql("SELECT * FROM " + dbname, mydb)
         df.plot.bar(x='name', y='salary', rot=0)
         plt.show()
+    elif choice == "4":
+         db.execute("USE " + dbname)
+         db.execute("SELECT * FROM " + dbname)
+         data = db.fetchall()
+         df = pd.DataFrame(data, columns=['id','name', 'age', 'salary', 'department'])
+         print(df)
+    elif choice == "5":
+        seach = input("Enter the ID to Search: ")
+        db.execute("USE " + dbname)
+        db.execute("SELECT * FROM " + dbname + " WHERE id = %s", (seach,))
+        data = db.fetchall()
+        df = pd.DataFrame(data, columns=['id','name', 'age', 'salary', 'department'])
+        print(df)
     else:
         print("Invalid choice")
